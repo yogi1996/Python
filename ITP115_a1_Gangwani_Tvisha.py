9c863ae4bf87dafbe9e1e59297e9f1ff07a69ca7
Tvisha Gangwani <gangwani@usc.edu>
2018-09-15 18:39:39 -0400

    added A1

---
+++ ITP115_a1_Gangwani_Tvisha.py
@@ -0,0 +1,25 @@
+# Tvisha Gangwani
+#ITP, Spring 2018
+#Lab A1
+#gangwani@usc.edu
+
+# taking in input
+
+animal = input("Enter an animal (Plural):")
+adj1= input("Enter an adjective:")
+adj2=input("Enter another adjective:")
+verb1=input("Enter a verb:")
+verb2=input("Enter a verb ending in 'ing':")
+num1=int(input("Enter a number:"))
+num2=int(input("Enter a second number:"))
+num3=int(input("Enter a third number:"))
+dec=float(input("Enter a number with a decimal:"))
+sum=num1+num2
+
+#NOTE TAKE IN INPUT AS INT AND FLOAT READ SLIDES
+
+#output
+print("Today I adopted \""+str(num1)+"\" pet \""+animal+"\". I learned that each animal needs \""+str(dec)+"\" hours of \""+verb2+"\" every day, and that they travel in groups of \""+str(num3)+"\".They are so \""+adj1+"\" that I decided to adopt \""+str(num2)+"\" more. Now I have \""+ str(sum) +"\" \""+animal+"\" and I am so \""+adj2+"\" that I want to \""+verb1+"\".")
+
+
+
